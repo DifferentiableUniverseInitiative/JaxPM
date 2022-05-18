5394abbed1f492a8840f13045ce0556c719686f5
Denise Lanzieri <72620117+dlanzieri@users.noreply.github.com>
2022-05-18 09:59:59 +0200

    Update jaxpm/pm.py
    Co-authored-by: Francois Lanusse <EiffL@users.noreply.github.com>

--- jaxpm/pm.py
+++ jaxpm/pm.py
@@ -75,6 +75,9 @@ def make_ode_fn(mesh_shape):
 def pgd_correction(pos, params):
     """
     improve the short-range interactions of PM-Nbody simulations with potential gradient descent method, based on https://arxiv.org/abs/1804.00671
+    args:
+      pos: particle positions [npart, 3]
+      params: [alpha, kl, ks] pgd parameters
     """
     kvec = fftk(mesh_shape)
 
