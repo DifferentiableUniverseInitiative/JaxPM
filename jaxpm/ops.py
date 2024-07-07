8b9287184ada2f0ee55aed318ce8943b35947b0c
Wassim KABALAN <wastondev@gmail.com>
2024-07-08 00:23:53 +0200

    Export operators

---
+++ jaxpm/ops.py
@@ -0,0 +1,34 @@
+import numpy as np
+from _src.spmd_config import pm_operators
+
+
+def fftn(arr):
+    return pm_operators.fftn(arr)
+
+
+def ifftn(arr):
+    return pm_operators.ifftn(arr)
+
+
+def halo_exchange(arr):
+    return pm_operators.halo_exchange(arr)
+
+
+def slice_pad(arr, pad_width):
+    return pm_operators.slice_pad(arr, pad_width)
+
+
+def slice_unpad(arr, pad_width):
+    return pm_operators.slice_unpad(arr, pad_width)
+
+
+def normal(shape, key, dtype='float32'):
+    return pm_operators.normal(shape, key, dtype)
+
+
+def fftk(shape, symmetric=True, finite=False, dtype=np.float32):
+    return pm_operators.fftk(shape, symmetric, finite, dtype)
+
+
+def generate_initial_positions(shape):
+    return pm_operators.generate_initial_positions(shape)
