a2811c06065a69804253843cacaed00a517e52a5
EiffL <fr.eiffel@gmail.com>
2024-07-09 14:54:34 -0400

    Applying formatting

--- jaxpm/nn.py
+++ jaxpm/nn.py
@@ -1,6 +1,7 @@
+import haiku as hk
 import jax
 import jax.numpy as jnp
-import haiku as hk
+
 
 def _deBoorVectorized(x, t, c, p):
     """
@@ -13,48 +14,47 @@ def _deBoorVectorized(x, t, c, p):
     c: array of control points
     p: degree of B-spline
     """
-    k = jnp.digitize(x, t) -1
-    
-    d = [c[j + k - p] for j in range(0, p+1)]
-    for r in range(1, p+1):
-        for j in range(p, r-1, -1):
-            alpha = (x - t[j+k-p]) / (t[j+1+k-r] - t[j+k-p])
-            d[j] = (1.0 - alpha) * d[j-1] + alpha * d[j]
+    k = jnp.digitize(x, t) - 1
+
+    d = [c[j + k - p] for j in range(0, p + 1)]
+    for r in range(1, p + 1):
+        for j in range(p, r - 1, -1):
+            alpha = (x - t[j + k - p]) / (t[j + 1 + k - r] - t[j + k - p])
+            d[j] = (1.0 - alpha) * d[j - 1] + alpha * d[j]
     return d[p]
 
 
 class NeuralSplineFourierFilter(hk.Module):
-  """A rotationally invariant filter parameterized by 
+    """A rotationally invariant filter parameterized by
   a b-spline with parameters specified by a small NN."""
 
-  def __init__(self, n_knots=8, latent_size=16, name=None):
+    def __init__(self, n_knots=8, latent_size=16, name=None):
+        """
+    n_knots: number of control points for the spline
     """
-    n_knots: number of control points for the spline  
-    """
-    super().__init__(name=name)
-    self.n_knots = n_knots
-    self.latent_size = latent_size
+        super().__init__(name=name)
+        self.n_knots = n_knots
+        self.latent_size = latent_size
 
-  def __call__(self, x, a):
-    """ 
+    def __call__(self, x, a):
+        """
     x: array, scale, normalized to fftfreq default
     a: scalar, scale factor
     """
 
-    net = jnp.sin(hk.Linear(self.latent_size)(jnp.atleast_1d(a)))
-    net = jnp.sin(hk.Linear(self.latent_size)(net))
+        net = jnp.sin(hk.Linear(self.latent_size)(jnp.atleast_1d(a)))
+        net = jnp.sin(hk.Linear(self.latent_size)(net))
 
-    w = hk.Linear(self.n_knots+1)(net) 
-    k = hk.Linear(self.n_knots-1)(net)
-    
-    # make sure the knots sum to 1 and are in the interval 0,1
-    k = jnp.concatenate([jnp.zeros((1,)),
-                        jnp.cumsum(jax.nn.softmax(k))])
+        w = hk.Linear(self.n_knots + 1)(net)
+        k = hk.Linear(self.n_knots - 1)(net)
 
-    w = jnp.concatenate([jnp.zeros((1,)),
-                         w])
+        # make sure the knots sum to 1 and are in the interval 0,1
+        k = jnp.concatenate([jnp.zeros((1, )), jnp.cumsum(jax.nn.softmax(k))])
 
-    # Augment with repeating points
-    ak = jnp.concatenate([jnp.zeros((3,)), k, jnp.ones((3,))])
+        w = jnp.concatenate([jnp.zeros((1, )), w])
 
-    return _deBoorVectorized(jnp.clip(x/jnp.sqrt(3), 0, 1-1e-4), ak, w, 3)
+        # Augment with repeating points
+        ak = jnp.concatenate([jnp.zeros((3, )), k, jnp.ones((3, ))])
+
+        return _deBoorVectorized(jnp.clip(x / jnp.sqrt(3), 0, 1 - 1e-4), ak, w,
+                                 3)
--- jaxpm/painting.py
+++ jaxpm/painting.py
@@ -1,98 +1,100 @@
 import jax
-import jax.numpy as jnp
 import jax.lax as lax
+import jax.numpy as jnp
 
-from jaxpm.kernels import fftk, cic_compensation
+from jaxpm.kernels import cic_compensation, fftk
+
 
 def cic_paint(mesh, positions, weight=None):
-  """ Paints positions onto mesh
+    """ Paints positions onto mesh
   mesh: [nx, ny, nz]
   positions: [npart, 3]
   """
-  positions = jnp.expand_dims(positions, 1)
-  floor = jnp.floor(positions)
-  connection = jnp.array([[[0, 0, 0], [1., 0, 0], [0., 1, 0], 
-                           [0., 0, 1], [1., 1, 0], [1., 0, 1], 
-                           [0., 1, 1], [1., 1, 1]]])
+    positions = jnp.expand_dims(positions, 1)
+    floor = jnp.floor(positions)
+    connection = jnp.array([[[0, 0, 0], [1., 0, 0], [0., 1, 0], [0., 0, 1],
+                             [1., 1, 0], [1., 0, 1], [0., 1, 1], [1., 1, 1]]])
 
-  neighboor_coords = floor + connection
-  kernel = 1. - jnp.abs(positions - neighboor_coords)
-  kernel = kernel[..., 0] * kernel[..., 1] * kernel[..., 2]  
-  if weight is not None:
+    neighboor_coords = floor + connection
+    kernel = 1. - jnp.abs(positions - neighboor_coords)
+    kernel = kernel[..., 0] * kernel[..., 1] * kernel[..., 2]
+    if weight is not None:
         kernel = jnp.multiply(jnp.expand_dims(weight, axis=-1), kernel)
-  
-  neighboor_coords = jnp.mod(neighboor_coords.reshape([-1,8,3]).astype('int32'), jnp.array(mesh.shape))
 
-  dnums = jax.lax.ScatterDimensionNumbers(
-    update_window_dims=(),
-    inserted_window_dims=(0, 1, 2),
-    scatter_dims_to_operand_dims=(0, 1, 2))
-  mesh = lax.scatter_add(mesh, 
-                         neighboor_coords, 
-                         kernel.reshape([-1,8]),
-                         dnums)
-  return mesh
+    neighboor_coords = jnp.mod(
+        neighboor_coords.reshape([-1, 8, 3]).astype('int32'),
+        jnp.array(mesh.shape))
+
+    dnums = jax.lax.ScatterDimensionNumbers(update_window_dims=(),
+                                            inserted_window_dims=(0, 1, 2),
+                                            scatter_dims_to_operand_dims=(0, 1,
+                                                                          2))
+    mesh = lax.scatter_add(mesh, neighboor_coords, kernel.reshape([-1, 8]),
+                           dnums)
+    return mesh
+
 
 def cic_read(mesh, positions):
-  """ Paints positions onto mesh
+    """ Paints positions onto mesh
   mesh: [nx, ny, nz]
   positions: [npart, 3]
-  """  
-  positions = jnp.expand_dims(positions, 1)
-  floor = jnp.floor(positions)
-  connection = jnp.array([[[0, 0, 0], [1., 0, 0], [0., 1, 0], 
-                           [0., 0, 1], [1., 1, 0], [1., 0, 1], 
-                           [0., 1, 1], [1., 1, 1]]])
+  """
+    positions = jnp.expand_dims(positions, 1)
+    floor = jnp.floor(positions)
+    connection = jnp.array([[[0, 0, 0], [1., 0, 0], [0., 1, 0], [0., 0, 1],
+                             [1., 1, 0], [1., 0, 1], [0., 1, 1], [1., 1, 1]]])
 
-  neighboor_coords = floor + connection
-  kernel = 1. - jnp.abs(positions - neighboor_coords)
-  kernel = kernel[..., 0] * kernel[..., 1] * kernel[..., 2]  
+    neighboor_coords = floor + connection
+    kernel = 1. - jnp.abs(positions - neighboor_coords)
+    kernel = kernel[..., 0] * kernel[..., 1] * kernel[..., 2]
 
-  neighboor_coords = jnp.mod(neighboor_coords.astype('int32'), jnp.array(mesh.shape))
+    neighboor_coords = jnp.mod(neighboor_coords.astype('int32'),
+                               jnp.array(mesh.shape))
 
-  return (mesh[neighboor_coords[...,0], 
-               neighboor_coords[...,1], 
-               neighboor_coords[...,3]]*kernel).sum(axis=-1)
+    return (mesh[neighboor_coords[..., 0], neighboor_coords[..., 1],
+                 neighboor_coords[..., 3]] * kernel).sum(axis=-1)
+
 
 def cic_paint_2d(mesh, positions, weight):
-  """ Paints positions onto a 2d mesh
+    """ Paints positions onto a 2d mesh
   mesh: [nx, ny]
   positions: [npart, 2]
   weight: [npart]
   """
-  positions = jnp.expand_dims(positions, 1)
-  floor = jnp.floor(positions)
-  connection = jnp.array([[0, 0], [1., 0], [0., 1], [1., 1]])
+    positions = jnp.expand_dims(positions, 1)
+    floor = jnp.floor(positions)
+    connection = jnp.array([[0, 0], [1., 0], [0., 1], [1., 1]])
 
-  neighboor_coords = floor + connection
-  kernel = 1. - jnp.abs(positions - neighboor_coords)
-  kernel = kernel[..., 0] * kernel[..., 1] 
-  if weight is not None:
-    kernel = kernel * weight[...,jnp.newaxis]
-  
-  neighboor_coords = jnp.mod(neighboor_coords.reshape([-1,4,2]).astype('int32'), jnp.array(mesh.shape))
+    neighboor_coords = floor + connection
+    kernel = 1. - jnp.abs(positions - neighboor_coords)
+    kernel = kernel[..., 0] * kernel[..., 1]
+    if weight is not None:
+        kernel = kernel * weight[..., jnp.newaxis]
 
-  dnums = jax.lax.ScatterDimensionNumbers(
-    update_window_dims=(),
-    inserted_window_dims=(0, 1),
-    scatter_dims_to_operand_dims=(0, 1))
-  mesh = lax.scatter_add(mesh, 
-                         neighboor_coords, 
-                         kernel.reshape([-1,4]),
-                         dnums)
-  return mesh
+    neighboor_coords = jnp.mod(
+        neighboor_coords.reshape([-1, 4, 2]).astype('int32'),
+        jnp.array(mesh.shape))
+
+    dnums = jax.lax.ScatterDimensionNumbers(update_window_dims=(),
+                                            inserted_window_dims=(0, 1),
+                                            scatter_dims_to_operand_dims=(0,
+                                                                          1))
+    mesh = lax.scatter_add(mesh, neighboor_coords, kernel.reshape([-1, 4]),
+                           dnums)
+    return mesh
+
 
 def compensate_cic(field):
-  """
+    """
   Compensate for CiC painting
   Args:
     field: input 3D cic-painted field
   Returns:
     compensated_field
   """
-  nc = field.shape
-  kvec = fftk(nc)
+    nc = field.shape
+    kvec = fftk(nc)
 
-  delta_k = jnp.fft.rfftn(field)
-  delta_k = cic_compensation(kvec) * delta_k
-  return jnp.fft.irfftn(delta_k)
+    delta_k = jnp.fft.rfftn(field)
+    delta_k = cic_compensation(kvec) * delta_k
+    return jnp.fft.irfftn(delta_k)
