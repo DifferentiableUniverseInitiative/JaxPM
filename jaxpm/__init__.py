6d8f130be7d5e46d31463aa129952104fbcf49dd
Wassim KABALAN <wastondev@gmail.com>
2024-07-09 02:34:40 +0200

    Import at the start to register everything

---
+++ jaxpm/__init__.py
@@ -0,0 +1,8 @@
+# Execute the register_operator functions
+import jaxpm._src.base_ops
+import jaxpm._src.painting_ops
+import jaxpm.ops
+import jaxpm.painting
+from jaxpm._src.spmd_config import SPMDConfig
+
+__all__ = ['SPMDConfig']
