4d944f01f270bfcc364429e43f4012013b3790b7
Wassim KABALAN <wastondev@gmail.com>
2024-10-22 12:10:18 -0400

    update demo script

--- scripts/distributed_pm.py
+++ scripts/distributed_pm.py
@@ -11,11 +11,12 @@ size = jax.device_count()
 import jax.numpy as jnp
 import jax_cosmo as jc
 import numpy as np
-from diffrax import Dopri5,LeapfrogMidpoint, ODETerm, ConstantStepSize, SaveAt, diffeqsolve
+from diffrax import (ConstantStepSize, Dopri5, LeapfrogMidpoint, ODETerm,
+                     SaveAt, diffeqsolve)
 from jax.experimental import mesh_utils
 from jax.sharding import Mesh, NamedSharding
 from jax.sharding import PartitionSpec as P
-
+from jax.experimental.multihost_utils import process_allgather
 from jaxpm.kernels import interpolate_power_spectrum
 from jaxpm.painting import cic_paint_dx
 from jaxpm.pm import linear_field, lpt, make_ode_fn
@@ -26,8 +27,10 @@ box_size = [float(size)] * 3
 snapshots = jnp.linspace(0.1, 1., 4)
 halo_size = 32
 pdims = (1, 1)
+mesh = None
+sharding = None
 if jax.device_count() > 1:
-    pdims = (8, 1)
+    pdims = (2, 4)
     devices = mesh_utils.create_device_mesh(pdims)
     mesh = Mesh(devices.T, axis_names=('x', 'y'))
     sharding = NamedSharding(mesh, P('x', 'y'))
@@ -40,19 +43,27 @@ def run_simulation(omega_c, sigma8):
     pk = jc.power.linear_matter_power(
         jc.Planck15(Omega_c=omega_c, sigma8=sigma8), k)
 
-    pk_fn = lambda x: interpolate_power_spectrum(x, k, pk)
+    pk_fn = lambda x: interpolate_power_spectrum(x, k, pk, sharding)
     # Create initial conditions
     initial_conditions = linear_field(mesh_shape,
                                       box_size,
                                       pk_fn,
+                                      sharding=sharding,
                                       seed=jax.random.PRNGKey(0))
 
     cosmo = jc.Planck15(Omega_c=omega_c, sigma8=sigma8)
 
     # Initial displacement
-    dx, p, _ = lpt(cosmo, initial_conditions, 0.1, halo_size=halo_size)
+    dx, p, _ = lpt(cosmo,
+                   initial_conditions,
+                   0.1,
+                   halo_size=halo_size,
+                   sharding=sharding)
+    return initial_conditions, cic_paint_dx(dx,
+                                            halo_size=halo_size,
+                                            sharding=sharding), None, None
     # Evolve the simulation forward
-    ode_fn = make_ode_fn(mesh_shape, halo_size=halo_size)
+    ode_fn = make_ode_fn(mesh_shape, halo_size=halo_size, sharding=sharding)
     term = ODETerm(
         lambda t, state, args: jnp.stack(ode_fn(state, t, args), axis=0))
     solver = LeapfrogMidpoint()
@@ -70,22 +81,17 @@ def run_simulation(omega_c, sigma8):
 
     # Return the simulation volume at requested
     states = res.ys
-    field = cic_paint_dx(dx, halo_size=halo_size)
+    field = cic_paint_dx(dx, halo_size=halo_size, sharding=sharding)
     final_fields = [
-        cic_paint_dx(state[0], halo_size=halo_size) for state in states
+        cic_paint_dx(state[0], halo_size=halo_size, sharding=sharding)
+        for state in states
     ]
 
     return initial_conditions, field, final_fields, res.stats
 
 
 # Run the simulation
-print(f"mesh {mesh}")
-if jax.device_count() > 1:
-    with mesh:
-        init, field, final_fields, stats = run_simulation(0.32, 0.8)
-
-else:
-    init, field, final_fields, stats = run_simulation(0.32, 0.8)
+init, field, final_fields, stats = run_simulation(0.32, 0.8)
 
 # # Print the statistics
 print(stats)
@@ -101,17 +107,14 @@ if is_on_cluster():
             np.save(f'final_field_{i}_{rank}.npy',
                     final_field.addressable_data(0))
 else:
-    indices = np.arange(len(init.addressable_shards)).reshape(
-        pdims[::-1]).transpose().flatten()
-    print(f"indices {indices}")
-    for i in np.arange(len(init.addressable_shards)):
+    gathered_init = process_allgather(init, tiled=True)
+    gathered_field = process_allgather(field, tiled=True)
+    np.save(f'initial_conditions.npy', gathered_init)
+    np.save(f'field.npy', gathered_field)
 
-        np.save(f'initial_conditions_{i}.npy', init.addressable_data(i))
-        np.save(f'field_{i}.npy', field.addressable_data(i))
-
-        if final_fields is not None:
-            for j, final_field in enumerate(final_fields):
-                np.save(f'final_field_{j}_{i}.npy',
-                        final_field.addressable_data(i))
+    if final_fields is not None:
+        for i, final_field in enumerate(final_fields):
+            gathered_final_field = process_allgather(final_field, tiled=True)
+            np.save(f'final_field_{i}.npy', gathered_final_field)
 
 print(f"Finished!!")
